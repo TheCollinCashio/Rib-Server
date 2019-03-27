7a71a128c2e00e5f4e1be4aaa187bfc57d382515
Collin Cashio <collin.cashio@selu.edu>
2019-03-27 15:14:37 -0500

    update the test file

--- test/index.js
+++ test/index.js
@@ -1,6 +1,4 @@
 let RibServer = require('../lib/RibServer').default
-// let PORT = process.argv[2] || 5000
-// let NAME = process.argv[3] || 'Collin'
 RibServer.startServer(5000, 'This is much easier to program')
 
 let myRib = new RibServer()
